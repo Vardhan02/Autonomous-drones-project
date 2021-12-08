15defb1660e89323cebd81add3cbdc2d5c76201c
ssk30-net <84185501+ssk30-net@users.noreply.github.com>
2021-12-08 22:48:11 +0530

    Face Detection Completed Successfully

--- Face tracker.py
+++ Face tracker.py
@@ -1,75 +1,76 @@
-import time
+# For face detection
+# Importing libraries
 import cv2
-import numpy as np
-#from djitellopy import tello
+import mediapipe as mp
+import time
 
-#me=tello.Tello()
-#me.connect()
-#print(me.get_battery())
+# Creating a class for face detection
+class FaceDetector():
+    # Intializing all face detection parameters
+    def __init__(self, minDetectionCon=0.5):
+        # the minimum confidence is set 
+        self.minDetectionCon = minDetectionCon
 
-#me.streamon()
-#me.takeoff()
-#me.send_rc_control(0,0,25,0)
-time.sleep(2.2)
+        self.mpFaceDetection = mp.solutions.face_detection
+        self.mpDraw = mp.solutions.drawing_utils # for drawing
+        self.faceDetection = self.mpFaceDetection.FaceDetection(self.minDetectionCon) # Intializes Face detection
 
-w,h=360,240
-fbRange=[6200,6800]
-pid=[0.4,0.4,0]
-pError =0
-def findFace(img):
-    faceCascade=cv2.CascadeClassifier("Autonomous-drones-project/haarcascade.py")
-    imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces=faceCacscade.detectMultiScale(imgGray,1.2,8)
+    def findFaces(self, img, draw=True):
 
-    myFaceListC=[]
-    myFaceListArea=[]
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # the output of imgRgb is stored in results
+        self.results = self.faceDetection.process(imgRGB)
+        bboxs = []
+        if self.results.detections:
+            for id, detection in enumerate(self.results.detections):
+                bboxC = detection.location_data.relative_bounding_box
+                ih, iw, ic = img.shape
+                bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                       int(bboxC.width * iw), int(bboxC.height * ih)
+                bboxs.append([id, bbox, detection.score])
+                if draw:
+                    img = self.fancyDraw(img,bbox)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
-        cx=x+w//2
-        cy=y+h//2
-        area= w*h
-        cv2.circle(img,(cx,cy),5,(0,0,255),cv2.FILLED)
-        myFaceListC.append([cx,cy])
-        myFaceListArea.append(area)
-    if len(myFaceListArea) !=0:
-        i=myFaceListArea.index(max(myFaceListArea))
-        return img,[myFaceListC[i],myFaceListArea[i]]
-    else:
-        return img,[[0,0],0]
+                    cv2.putText(img, f'{int(detection.score[0] * 100)}%',
+                            (bbox[0], bbox[1] - 20), cv2.FONT_HERSHEY_PLAIN,
+                            2, (255, 0, 255), 2)
+        return img, bboxs
 
-def trackFace(info,w,pid,pError):
-    area = info[1]
-    x,y=info[0]
-    fb=0
-    error=x-w//2
-    speed=pid[0]*error+pid[1]*(error-pError)
-    speed=int(np.clip(speed,-100,100))
+    def fancyDraw(self, img, bbox, l=30, t=5, rt= 1):
+        x, y, w, h = bbox
+        x1, y1 = x + w, y + h
+
+        cv2.rectangle(img, bbox, (255, 0, 255), rt)
+        # Top Left  x,y
+        cv2.line(img, (x, y), (x + l, y), (255, 0, 255), t)
+        cv2.line(img, (x, y), (x, y+l), (255, 0, 255), t)
+        # Top Right  x1,y
+        cv2.line(img, (x1, y), (x1 - l, y), (255, 0, 255), t)
+        cv2.line(img, (x1, y), (x1, y+l), (255, 0, 255), t)
+        # Bottom Left  x,y1
+        cv2.line(img, (x, y1), (x + l, y1), (255, 0, 255), t)
+        cv2.line(img, (x, y1), (x, y1 - l), (255, 0, 255), t)
+        # Bottom Right  x1,y1
+        cv2.line(img, (x1, y1), (x1 - l, y1), (255, 0, 255), t)
+        cv2.line(img, (x1, y1), (x1, y1 - l), (255, 0, 255), t)
+        return img
 
 
-    if area>fbRange[0] and area<fbRange[1]:
-        fb=0
-    elif area>fbRange[2]:
-        fb=-20
-    elif area<fbRange[0] and area!=0:
-        fb=20
-    if x==0:
-        speed=0
-        error=0
-    #print(speed,fb)
-    #me.send_rc_control(0,fb,0,speed)
-    return error
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = FaceDetector()
+    while True:
+        success, img = cap.read()
+        img, bboxs = detector.findFaces(img)
+        print(bboxs)
 
-cap=cv2.VideoCapture(1)
-while True:
-    success,img=cap.read()
-    img=cap.get_frame_read().frame
-    img=cv2.resize(img,(w,h))
-    img,info = findFace(img)
-    pError=trackFace( info, w, pid, pError)
-    #print("Center",info[0],"Area",info[1])
-    cv2.imshow("Output",img)
-    if cv2.waitKey(1) &0xFF==ord('q'):
-        #me.land()
-        break
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
+
+if __name__ == "__main__":
+    main()
