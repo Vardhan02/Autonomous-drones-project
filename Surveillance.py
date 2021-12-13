c9cc8d16db373b3205a5769d2c191159ac832af6
Vardhan Viswanath <vardhanviswa@gmail.com>
2021-12-13 18:10:09 +0530

    Completed

--- Surveillance.py
+++ Surveillance.py
@@ -1,14 +1,17 @@
-from djitellopy import tello
+#from djitellopy import tello
 import KeyPressModule as kp
 import time
 import cv2
 
 kp.init()
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
+#me = tello.Tello()
+#me.connect()
+#print(me.get_battery())
 global img
-me.streamon()
+#me.streamon()
+cap = cv2.VideoCapture(0)
+cap.set(3,640) # For setting width
+cap.set(4,480) # For setting height
 
 def getKeyboardInput():
     lr, fb, ud, yv = 0, 0, 0, 0
@@ -26,8 +29,8 @@ def getKeyboardInput():
     if kp.getKey("a"): yv = -speed
     elif kp.getKey("d"): yv = speed
 
-    if kp.getKey("q"): yv = me.land(); time.sleep(3)
-    if kp.getKey("e"): yv = me.takeoff()
+    if kp.getKey("q"): yv = time.sleep(3); #me.land()
+    #if kp.getKey("e"): yv = me.takeoff()
 
     if kp.getKey('z'):
         cv2.imwrite(f'Resources/Images/{time.time()}.jpg',img)
@@ -37,8 +40,8 @@ def getKeyboardInput():
 
 while True:
     vals = getKeyboardInput()
-    me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
-    img = me.get_frame_read().frame
-    img = cv2.resize(img, (360,240))
+    #me.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    #img = me.get_frame_read().frame
+    success,img=cap.read()
     cv2.imshow("Image", img)
     cv2.waitKey(1)
