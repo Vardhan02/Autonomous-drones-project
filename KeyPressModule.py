85ba32dd5f884da4efeb246ba5f39b00429bdb11
Vardhan Viswanath <vardhanviswa@gmail.com>
2021-12-16 14:20:42 +0530

    Completed

--- KeyPressModule.py
+++ KeyPressModule.py
@@ -21,6 +21,10 @@ def main():
         print("Left key pressed")
     if getKey("RIGHT"):
         print("Right key pressed")
+    if getKey("UP"):
+        print("Up key pressed")
+    if getKey("DOWN"):
+        print("Down key pressed")
 
 
 if __name__ == '__main__':
