c5fb3c28d453f53a940f7168877cda46e24d4a63
ssk30-net <84185501+ssk30-net@users.noreply.github.com>
2021-12-23 11:01:54 +0530

    Completed sensors.py,env.py,main.py MADE IT WORK BOIS

--- Slam/main.py
+++ Slam/main.py
@@ -1,27 +1,13 @@
 from Slam import env,Sensors
 import pygame
+import math
 
-environment= env.buildEnvironment((600, 1200))
-environment.orginalMap=environment.map.copy()
-laser=Sensors.LaserSensor(200,environment.orginalMap,uncertainty=(0.5,0.01))
-environment.map.fill((0,0,0))
-environment.infomap=environment.map.copy()
-running=True
+environment=env.buildEnvironment((600,1200))
+running = True
 
 while running:
-    sensorON=False
+
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
-            running=False
-        if pygame.mouse.get_focused():
-            sensorON=True
-        elif not pygame.mouse.get_focused():
-            sensorON=False
-    if sensorON:
-        position=pygame.mouse.get_pos()
-        laser.position=position
-        sensor_data=laser.sense_obstacles()
-        environment.dataStorage(sensor_data)
-        environment.show_sensorData()
-    environment.map.blit(environment.infomap,(0,0))
+            running= False
     pygame.display.update()
